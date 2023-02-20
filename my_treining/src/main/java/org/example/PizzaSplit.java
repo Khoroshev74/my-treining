03984d876dda12b117f44f5bc69ed0efb1a5ce18
Oleksandr Khoroshev <rtg2603741@gmail.com>
2023-02-20 14:03:20 +0200

    PizzaSplit

--- my_treining/src/main/java/org/example/PizzaSplit.java
+++ my_treining/src/main/java/org/example/PizzaSplit.java
@@ -1,2 +1,23 @@
-package org.example;public class PizzaSplit {
+package org.example;
+//Посчитать минимальное количество пиц, с учетом количества кусочков и кол ва людей.
+//На ввод количество людей и кусочков
+import java.util.Scanner;
+
+public class PizzaSplit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int people = sc.nextInt();
+        int pice = sc.nextInt();
+        int countPice = pice;
+        int pizza = 1;
+        while (people > 0) {
+            pice = countPice;
+            pice *= pizza;
+            if (pice % people == 0) {
+                break;
+            }
+            pizza ++;
+        }
+        System.out.println(pizza);
+    }
 }
