4b1cd3c40b073b46120120acf93f63e427e2b8de
Oleksandr Khoroshev <rtg2603741@gmail.com>
2023-03-08 15:47:45 +0200

    Matrix add

--- my_treining/src/main/java/org/example/Matrix.java
+++ my_treining/src/main/java/org/example/Matrix.java
@@ -1,2 +1,41 @@
-package org.example;public class Matrix {
+package org.example;
+
+public class Matrix {
+    public static void main(String[] args) {
+        int [][] oneMatrix = new int[3][3];
+        int [][] twoMatrix = new int[3][3];
+        int [][] resMatrix = new int[3][3];
+        oneMatrix = fillMatrix(oneMatrix);
+        twoMatrix = fillMatrix(twoMatrix);
+        outputMatrix(oneMatrix);
+        System.out.println("++++++++++");
+        outputMatrix(twoMatrix);
+        //Складываем две матрицы
+        for (int i = 0; i < resMatrix.length; i++){
+            for (int j = 0; j < resMatrix[i].length; j++){
+                resMatrix[i][j] = oneMatrix[i][j] + twoMatrix[i][j];
+            }
+        }
+        System.out.println("===========");
+        outputMatrix(resMatrix);
+
+
+    }
+    //Заполняем первые две матрицы, создаем метод fillMatrix
+    static int[][] fillMatrix (int tmpMatrix[][]){
+        for (int i = 0; i < tmpMatrix.length; i++){
+            for (int j = 0; j < tmpMatrix[i].length; j++){
+                tmpMatrix[i][j] = (int) (Math.random()*10);
+            }
+        }
+        return tmpMatrix;
+    }
+    static void outputMatrix (int tmpMatrix[][]){
+        for (int i = 0; i < tmpMatrix.length; i++){
+            for (int j = 0; j < tmpMatrix[i].length; j++){
+                System.out.print(tmpMatrix[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
 }
