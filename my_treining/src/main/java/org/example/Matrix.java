240c9d1e34b017785458806a107db07fcc681540
Oleksandr Khoroshev <rtg2603741@gmail.com>
2023-03-08 16:30:52 +0200

    Transponirovanie matrix

--- my_treining/src/main/java/org/example/Matrix.java
+++ my_treining/src/main/java/org/example/Matrix.java
@@ -5,6 +5,7 @@ public class Matrix {
         int [][] oneMatrix = new int[3][3];
         int [][] twoMatrix = new int[3][3];
         int [][] resMatrix = new int[3][3];
+        int [][] trMatrix = new int[3][3];
         oneMatrix = fillMatrix(oneMatrix);
         twoMatrix = fillMatrix(twoMatrix);
         outputMatrix(oneMatrix);
@@ -19,8 +20,17 @@ public class Matrix {
         System.out.println("===========");
         outputMatrix(resMatrix);
 
-
+        //Транспонируем полученную матрицу при сложении
+        for (int i = 0; i < trMatrix.length; i++){
+            for (int j = 0; j < trMatrix[i].length; j++){
+                trMatrix[i][j] = resMatrix[j][i];
+            }
+        }
+        System.out.println("tttttttttttt");
+        outputMatrix(trMatrix);
     }
+
+
     //Заполняем первые две матрицы, создаем метод fillMatrix
     static int[][] fillMatrix (int tmpMatrix[][]){
         for (int i = 0; i < tmpMatrix.length; i++){
